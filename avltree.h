2c0feddfa241583fc9839f0acfccee8f2478d3a8
Fredrik Österdahl <fredrik_ostdahl@hotmail.com>
2017-10-25 12:39:35 +0200

    Update avltree.h
    Added tree traversals. Also changed name to avl_tree

--- avltree.h
+++ avltree.h
@@ -1,46 +1,48 @@
 
-typedef struct n
+typedef struct tree_node_struct
 {
 	int value;
 	struct tree_node * lchild;
 	struct tree_node * rchild;
 } tree_node;
 
-typedef struct t
+typedef struct avl_tree_struct
 {
 	int size;
 	tree_node * tree;
-} tree;
+} avl_tree;
 
 /**** BASIC OPERATIONS BEGIN ****/
 
-	tree * create_tree();
+	avl_tree * create_tree();
 
-	int add_element(tree * root, int value);
-	int remove_element(tree * root, int value);
+	int add_element(avl_tree * root, int value);
+	int remove_element(avl_tree * root, int value);
 
 /**** BASIC OPERATIONS END ***/
 
 /**** META DATA FUNCTIONS BEGIN ****/
 
-	int tree_empty(tree * root);
-	int tree_size(tree * root);
-	int largest_value_in_tree(tree * root);
-	int smallest_value_in_tree(tree * root);
-	int tree_is_complete(tree * root);
-	int value_exists(tree * root, int value);
-	int value_in_tree(tree_node * node, int value);
-	int complete_tree_size(tree * root);
+	int tree_empty(avl_tree * root);
+	int tree_size(avl_tree * root);
+	int largest_value_in_tree(avl_tree * root);
+	int smallest_value_in_tree(avl_tree * root);
+	int tree_is_complete(avl_tree * root);
+	int value_exists(avl_tree * root, int value);
+	int complete_tree_size(avl_tree * root);
+	int tree_height(avl_tree * root);
 	
-	tree_node ** inorder_node_list(tree * root);
+	tree_node ** inorder_node_list(avl_tree * root);
+	tree_node ** preorder_node_list(avl_tree * root);
+	tree_node ** postorder_node_list(avl_tree * root);
 
 /**** META DATA FUNCTIONS END ****/
 
 
 /**** PRINT FUNCTIONS BEGIN ****/
 
-	void array_print(tree * root);
-	void pretty_print_tree(tree * root);
+	void array_print(avl_tree * root);
+	void pretty_print_tree(avl_tree * root);
 
 /**** PRINT FUNCTIONS END ****/
 
